c8ddfd150decbf95dff181aa3be0962c66b6b1b1
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2020-08-06 00:24:36 +0800

    Initial Commit

---
+++ ARLaboratory/ARLaboratoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARLaboratoryApp.swift
+//  ARLaboratory
+//
+//  Created by 陈俊杰 on 2020/8/6.
+//
+
+import SwiftUI
+
+@main
+struct ARLaboratoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARLaboratory/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ARLaboratory
+//
+//  Created by 陈俊杰 on 2020/8/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
