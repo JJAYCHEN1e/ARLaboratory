e7b6265cc7d86742abf75c880a94ecf9d89877b7
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2020-08-11 21:18:28 +0800

    Update UINavigationController+Swipe.swift

--- ARLaboratory/Extension/UINavigationController+Swipe.swift
+++ ARLaboratory/Extension/UINavigationController+Swipe.swift
@@ -10,13 +10,13 @@ import UIKit
 /// Allow swipe back gesture when use .navigationBarHidden(true)
 /// Reference:
 /// https://stackoverflow.com/questions/59921239/hide-navigation-bar-without-losing-swipe-back-gesture-in-swiftui
-extension UINavigationController: UIGestureRecognizerDelegate {
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        interactivePopGestureRecognizer?.delegate = self
-    }
-    
-    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        return viewControllers.count > 1
-    }
-}
+//extension UINavigationController: UIGestureRecognizerDelegate {
+//    override open func viewDidLoad() {
+//        super.viewDidLoad()
+//        interactivePopGestureRecognizer?.delegate = self
+//    }
+//    
+//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        return viewControllers.count > 1
+//    }
+//}
