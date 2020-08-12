f60e216b6b19ef3acb995a7ff7042db7359600ba
徐滔锴 <xavierxu@Xaviers-MacBook-Pro-16-inches.local>
2020-08-13 00:51:25 +0800

    新增积分和选择逻辑

--- ARLaboratory/View/Q&A Page/CardViews.swift
+++ ARLaboratory/View/Q&A Page/CardViews.swift
@@ -7,16 +7,22 @@
 
 import SwiftUI
 
-struct CardViews: View {
-    var body: some View {
-        ZStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            ProblemCard(problem: problems[0]).scaleEffect(x:0.93,y: 0.85 ,anchor: .center).offset(x: 58).opacity(0.4)
-            ProblemCard(problem: problems[0]).scaleEffect(x:0.98,y: 0.93 ,anchor: .center).offset(x: 27).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-            ProblemCard(problem: problems[0])
-            
-  
 
+struct CardViews: View {
+    @State var score : Int = 0
+    @State var offset : Int = 0
+    var body: some View {
+        
+        VStack {
+            Text(String.init(score))
+            ZStack {
+                ProblemCard(problem: problems[0], score: $score).scaleEffect(x:0.93,y: 0.85 ,anchor: .center).offset(x: 58).opacity(0.3)
+                ProblemCard(problem: problems[0], score: $score).scaleEffect(x:0.98,y: 0.93 ,anchor: .center).offset(x: 27).opacity(0.9)
+                ProblemCard(problem: problems[0], score: $score)
+                
+      
+
+            }
         }
     }
 }
--- ARLaboratory/View/Q&A Page/ChoiceCell.swift
+++ ARLaboratory/View/Q&A Page/ChoiceCell.swift
@@ -8,25 +8,49 @@
 import SwiftUI
 
 struct ChoiceCell: View {
-    @State var content : String
-    var isTure: Bool = true
-    
+    var content : String
+    var index : Int
+    @State var selected: Bool = false
+    @State var isRightAnswer: Bool = false
+    @Binding var hasPressed : Bool
+    @State var imageStr : String = "unselected"
+    @Binding var chosenIndex : Int
+
     var body: some View {
-            HStack {
-                Text(content)
-                Spacer()
-                (isTure ? Image("correct") : Image("fault"))
-                    .resizable()
-                    .frame(width: 18, height: 18)
-                    
-            }
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 9)
-                        .stroke(
-                            Color(#colorLiteral(red: 0.8352941176, green: 0.8352941176, blue: 0.9647058824, alpha: 1)),lineWidth: 2.5)
-                        )
+            
+       
         
+   
+            HStack {
+                    Text(content)
+                    Spacer()
+                    (hasPressed ? (isRightAnswer ? Image("correct") : selected ? Image("false") : Image("unselected")) : Image("unselected"))
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                        
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 9)
+                            .stroke(
+                                (hasPressed ? (isRightAnswer ? Color(#colorLiteral(red: 0.2745098039, green: 0.2745098039, blue: 0.8549019608, alpha: 1)) : selected ? Color(#colorLiteral(red: 0.8745098039, green: 0.5019607843, blue: 0.7450980392, alpha: 1)) : Color(#colorLiteral(red: 0.8352941176, green: 0.8352941176, blue: 0.9647058824, alpha: 1))) : Color(#colorLiteral(red: 0.8352941176, green: 0.8352941176, blue: 0.9647058824, alpha: 1))),lineWidth: 2.5)
+            )
+            .background(Color.white)
+            .onTapGesture{
+                print("1")
+                withAnimation(){
+                    if(!hasPressed){
+                        hasPressed = true
+                        selected = true
+                        chosenIndex = index
+                        
+                    }
+                    
+                }
+            }
+        
+           
+           
 
 
 
@@ -36,7 +60,15 @@ struct ChoiceCell: View {
 
 struct ChoiceCell_Previews: PreviewProvider {
     static var previews: some View {
-        ChoiceCell(content: "Test")
+        ChoiceCell(content: "Test", index: 0,hasPressed: .constant(false), imageStr: "unselected", chosenIndex: .constant(0))
     }
+}
+
+
+struct unselected : ViewModifier {
+    func body(content: Content) -> some View {
+           content
+               
+       }
 }
 
