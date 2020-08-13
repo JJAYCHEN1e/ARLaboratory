7938718dad9cf937acaef8281e3aa8781902c445
徐滔锴 <xavierxu@Xaviers-MacBook-Pro-16-inches.local>
2020-08-14 01:57:44 +0800

    完成大部分答题卡片，暂存一下

--- ARLaboratory/View/Q&A Page/CardViews.swift
+++ ARLaboratory/View/Q&A Page/CardViews.swift
@@ -7,90 +7,79 @@
 
 import SwiftUI
 
+class ViewTypeInfo {
+    var scaleX : Double
+    var scaleY : Double
+    var offsetX: Int
+    var opacity: Double
+    var zIndex: Int
+    init(scaleX : Double,scaleY : Double,offsetX: Int,opacity: Double,zIndex: Int) {
+        self.scaleX = scaleX
+        self.scaleY = scaleY
+        self.offsetX = offsetX
+        self.opacity = opacity
+        self.zIndex = zIndex
+    }
+}
+
 
 struct CardViews: View {
     @State var score : Int = 0
     @State var offset : Int = 0
+    @State var experimentName : String
     var body: some View {
         
         VStack {
             Text(String.init(score))
             Text(String.init(offset))
             ZStack {
-                ProblemCard(problem: problems[(0+offset)%4], score: $score).selectModifier(offset: offset+3)
-                ProblemCard(problem: problems[(1+offset)%4], score: $score).selectModifier(offset: offset+2)
-                ProblemCard(problem: problems[(2+offset)%4], score: $score).selectModifier(offset: offset+1)
-                ProblemCard(problem: problems[(3+offset)%4], score: $score).selectModifier(offset: offset)
+                ForEach(problems.indices,id : \.self){ i in
+                    let viewTypeInfo = selectViewType(offset: i - offset)
+                    ProblemCard(problemIndex: i, experimentName: experimentName, problem: problems[(i+offset)%4], score: $score, offset: $offset)
+                        .scaleEffect(x: CGFloat(viewTypeInfo.scaleX), y: CGFloat(viewTypeInfo.scaleY), anchor: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .offset(x: CGFloat(viewTypeInfo.offsetX), y: 0)
+                        .opacity(viewTypeInfo.opacity)
+                        .zIndex(Double(viewTypeInfo.zIndex))
+                }
             }
-            Button(action: {
-                offset = offset + 1
-            } ) {
-                Text("测试").foregroundColor(.white)
-                    .frame(width: 298, height: 44)
-                    .background(Color(#colorLiteral(red: 0.2745098039, green: 0.2745098039, blue: 0.8549019608, alpha: 1)))
-                    .cornerRadius(12)
+           
             }
-            .animation(.easeInOut)
-            }
+        .animation(.easeInOut)
+
 
             
         }
     }
 
 
-
-
-extension View{
-    @ViewBuilder func selectModifier(offset : Int) -> some View{
-        switch (offset%4) {
-        case 0:
-             self.modifier(firstCard())
-        case 1:
-             self.modifier(secondCard())
-        case 2:
-             self.modifier(thirdCard())
-        default:
-             self.modifier(candidateCard())
-
+func selectViewType(offset : Int) -> ViewTypeInfo {
+    switch offset {
+    case 0:
+        return first
+    case 1:
+        return second
+    case 2:
+        return third
+    default:
+        if offset < 0 {
+            return candidateBig
         }
-            
-        
+        return candidateSmall
     }
 }
+
+
+
+let candidateSmall = ViewTypeInfo(scaleX: 0.93, scaleY: 0.78, offsetX: 70, opacity: 0, zIndex: 1)
+let first = ViewTypeInfo(scaleX: 1, scaleY: 1, offsetX: 0, opacity: 1, zIndex: 4)
+let second = ViewTypeInfo(scaleX: 0.98, scaleY: 0.93, offsetX: 27, opacity: 0.9, zIndex: 3)
+let third = ViewTypeInfo(scaleX: 0.93, scaleY: 0.85, offsetX: 58, opacity: 0.3, zIndex: 2)
+let candidateBig = ViewTypeInfo(scaleX: 1.1, scaleY: 1.1, offsetX: -50, opacity: -1, zIndex: 5)
+
+
 struct CardViews_Previews: PreviewProvider {
     static var previews: some View {
-        CardViews()
+        CardViews( experimentName: "小球碰撞验证动量守恒定律")
     }
 }
 
-struct firstCard : ViewModifier {
-    
-    func body(content : Content) -> some View {
-        content.zIndex(4.0)
-        
-    }
-}
-
-struct secondCard : ViewModifier {
-    
-    func body(content : Content) -> some View {
-        content.scaleEffect(x:0.98,y: 0.93 ,anchor: .center).offset(x: 27).opacity(0.9).zIndex(3.0)
-        
-    }
-}
-
-struct thirdCard : ViewModifier {
-    
-    func body(content : Content) -> some View {
-        content.scaleEffect(x:0.93,y: 0.85 ,anchor: .center).offset(x: 58).opacity(0.3).zIndex(2.0)
-        
-    }
-}
-
-struct candidateCard : ViewModifier {
-    
-    func body(content : Content) -> some View {
-        content.scaleEffect(x:0.93,y: 0.78 ,anchor: .center).offset(x: 70).opacity(0).zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-        
-    }
-}
