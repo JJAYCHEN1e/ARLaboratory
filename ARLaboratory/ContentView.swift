8c12ffd15c0b46fe4d17777e53955464cec81f39
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2020-08-13 10:47:43 +0800

    Refix ScrollView stuck issue in home page

--- ARLaboratory/ContentView.swift
+++ ARLaboratory/ContentView.swift
@@ -11,33 +11,31 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                Rectangle()
-                    .foregroundColor(Color(#colorLiteral(red: 0.9490196078, green: 0.9529411765, blue: 0.9607843137, alpha: 0.5)))
-                    .edgesIgnoringSafeArea(.all)
-                    .overlay(
-                        ScrollView {
-                            VStack(alignment: .leading, spacing: 0) {
-                                ColumnHeadView(title: "今日推荐", subtitle: "Today", destination: SubjectView())
-                                    .padding(.horizontal)
-                                RecommandColumn()
-
-                                ColumnHeadView(title: "学科分类", subtitle: "All", destination: SubjectView())
-                                    .padding(.horizontal, 24)
-                                SubjectColumn()
-
-                                ColumnHeadView(title: "最爱实验", subtitle: "My", destination: SubjectView())
-                                    .padding(.horizontal, 24)
-                                FavoriteColumn()
-                                
-                                ColumnHeadView(title: "我的导入", subtitle: "Import", destination: SubjectView())
-                                    .padding(.horizontal, 24)
-                                FavoriteColumn()
-                            }
+            Rectangle()
+                .foregroundColor(Color(#colorLiteral(red: 0.9490196078, green: 0.9529411765, blue: 0.9607843137, alpha: 0.5)))
+                .edgesIgnoringSafeArea(.all)
+                .overlay(
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 0) {
+                            ColumnHeadView(title: "今日推荐", subtitle: "Today", destination: SubjectView())
+                                .padding(.horizontal)
+                            RecommandColumn()
+                            
+                            ColumnHeadView(title: "学科分类", subtitle: "All", destination: SubjectView())
+                                .padding(.horizontal, 24)
+                            SubjectColumn()
+                            
+                            ColumnHeadView(title: "最爱实验", subtitle: "My", destination: SubjectView())
+                                .padding(.horizontal, 24)
+                            FavoriteColumn()
+                            
+                            ColumnHeadView(title: "我的导入", subtitle: "Import", destination: SubjectView())
+                                .padding(.horizontal, 24)
+                            FavoriteColumn()
                         }
-                    )
-            }
-            .navigationBarHidden(true)
+                    }
+                )
+                .navigationBarHidden(true)
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
